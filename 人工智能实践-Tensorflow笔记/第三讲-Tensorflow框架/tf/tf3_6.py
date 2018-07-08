d4711c3cfe26a19738464b93c9a349265ad6c8f7
qrzbing <ikav@qq.com>
2018-07-08 17:59:01 +0800

    tensorflow - 3 finished

--- 人工智能实践-Tensorflow笔记/第三讲-Tensorflow框架/tf/tf3_6.py
+++ 人工智能实践-Tensorflow笔记/第三讲-Tensorflow框架/tf/tf3_6.py
@@ -1,9 +1,9 @@
 #coding:utf-8
 #0导入模块，生成模拟数据集。
 import tensorflow as tf
-import numpy as np
-BATCH_SIZE = 8
-SEED = 23455
+import numpy as np # numpy 模块是 python 科学计算模块
+BATCH_SIZE = 128 # 一次喂入神经网络多少组数据，该数据不可过大
+SEED = 23455 # 方便教学
 
 #基于seed产生随机数
 rdm = np.random.RandomState(SEED)
@@ -11,29 +11,31 @@ rdm = np.random.RandomState(SEED)
 X = rdm.rand(32,2)
 #从X这个32行2列的矩阵中 取出一行 判断如果和小于1 给Y赋值1 如果和不小于1 给Y赋值0 
 #作为输入数据集的标签（正确答案） 
-Y_ = [[int(x0 + x1 < 1)] for (x0, x1) in X]
+Y_ = [[int(x0 + x1 < 1)] for (x0, x1) in X] # 人为设定，体积加重量小于1的零件认为是合格的
+# 虚拟的数据集
 print "X:\n",X
 print "Y_:\n",Y_
 
 #1定义神经网络的输入、参数和输出,定义前向传播过程。
-x = tf.placeholder(tf.float32, shape=(None, 2))
-y_= tf.placeholder(tf.float32, shape=(None, 1))
+x = tf.placeholder(tf.float32, shape=(None, 2)) # 输入的特征
+y_= tf.placeholder(tf.float32, shape=(None, 1)) # 标准答案，每个标签只有1个元素
 
-w1= tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
-w2= tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
-
+w1= tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1)) # w1对应x，两层。
+w2= tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1)) # w2对应y_，一列。
+# 中间隐藏层用3个神经元 seed = 1 为了教学方便
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
 
 #2定义损失函数及反向传播方法。
-loss_mse = tf.reduce_mean(tf.square(y-y_)) 
-train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
-#train_step = tf.train.MomentumOptimizer(0.001,0.9).minimize(loss_mse)
-#train_step = tf.train.AdamOptimizer(0.001).minimize(loss_mse)
+loss_mse = tf.reduce_mean(tf.square(y-y_))  # 使用均方误差计算 loss
+# train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
+# 使用梯度下降实现训练过程，学习率为0.001
+# train_step = tf.train.MomentumOptimizer(0.001,0.9).minimize(loss_mse)
+train_step = tf.train.AdamOptimizer(0.001).minimize(loss_mse)
 
 #3生成会话，训练STEPS轮
 with tf.Session() as sess:
-    init_op = tf.global_variables_initializer()
+    init_op = tf.global_variables_initializer() 
     sess.run(init_op)
     # 输出目前（未经训练）的参数取值。
     print "w1:\n", sess.run(w1)
@@ -41,12 +43,15 @@ with tf.Session() as sess:
     print "\n"
     
     # 训练模型。
-    STEPS = 3000
+    STEPS = 8000 # 训练3k轮
     for i in range(STEPS):
         start = (i*BATCH_SIZE) % 32
         end = start + BATCH_SIZE
+        # 每轮从 X 的数据集和 Y 的标签抽取相应的从 start 到 end 个特征和标签喂入神经网络中
+        # sess.run 执行训练过程   
         sess.run(train_step, feed_dict={x: X[start:end], y_: Y_[start:end]})
         if i % 500 == 0:
+            # 每 500 轮打印一次 loss 值
             total_loss = sess.run(loss_mse, feed_dict={x: X, y_: Y_})
             print("After %d training step(s), loss_mse on all data is %g" % (i, total_loss))
     
@@ -55,6 +60,7 @@ with tf.Session() as sess:
     print "w1:\n", sess.run(w1)
     print "w2:\n", sess.run(w2)
 
+ 
 """
 X:
 [[ 0.83494319  0.11482951]
